7ebd434488b14997ba9a20c08f34fd7e5aea5182
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-12-28 16:53:04 -0500

    Initial Commit

---
+++ Guess The Drawing/Guess_The_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_DrawingApp.swift
+//  Guess The Drawing
+//
+//  Created by Leonard McCook-Carr on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
