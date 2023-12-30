71fa61aeed2edfc167982a9f9d457fa6ce2e513a
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-12-30 02:11:05 -0500

    Create MenuView and MatchManager files

---
+++ Guess The Drawing/MatchManager.swift
@@ -0,0 +1,8 @@
+//
+//  MatchManager.swift
+//  Guess The Drawing
+//
+//  Created by Leonard McCook-Carr on 12/30/23.
+//
+
+import Foundation
---
+++ Guess The Drawing/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  Guess The Drawing
+//
+//  Created by Leonard McCook-Carr on 12/30/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
