5ebf030aeebdd6a6922f1b7919568a1dc403c788
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-09 08:17:31 -0500

    Create DrawingView file and import PencilKit

---
+++ Guess The Drawing/DrawingView.swift
@@ -0,0 +1,19 @@
+//
+//  DrawingView.swift
+//  Guess The Drawing
+//
+//  Created by Leonard McCook-Carr on 1/9/24.
+//
+
+import SwiftUI
+import PencilKit
+
+struct DrawingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrawingView()
+}
