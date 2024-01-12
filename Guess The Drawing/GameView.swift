7a18dff56b1743d070d7327081e38a08bd2b163f
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-12 06:10:34 -0500

    Add eraser button

--- Guess The Drawing/GameView.swift
+++ Guess The Drawing/GameView.swift
@@ -34,10 +34,37 @@ struct GameView: View {
                            .aspectRatio(1, contentMode: .fit)
                            .overlay(
                               RoundedRectangle(cornerRadius: 10)
-                           .stroke(.black, lineWidth: 10))
+                           .stroke(.black, lineWidth: 10)
+                           
+                           )
                         
+                        VStack {
+                            HStack {
+                                Spacer()
+                                
+                                if matchManager.currentlyDrawing {
+                                    Button {
+                                        eraserEnabled.toggle()
+                                    } label: {
+                                        Image(systemName: eraserEnabled ?
+                                              "eraser.fill" : "eraser")
+                                              .font(.title)
+                                              .foregroundStyle(Color("primaryPurple"))
+                                              .padding(.top, 10)
+                                        
+                                    }
+                                }
+                            }
+                            
+                            Spacer()
+                        }
+                        .padding()
                     }
+                    
+                    pastGuesses
                 }
+                .padding(.horizontal, 30)
+                .ignoresSafeArea(.keyboard, edges: .bottom)
             }
         }
     }
@@ -69,6 +96,44 @@ struct GameView: View {
             }
         }
         .padding(.vertical, 15)
+    }
+    
+    var pastGuesses: some View {
+        ScrollView {
+            ForEach(matchManager.pastGuesses) { guess in
+                HStack {
+                    Text(guess.message)
+                        .font(.title2)
+                        .bold(guess.correct)
+                    
+                    if guess.correct {
+                        Image(systemName: "hand.thumbsup.fill")
+                            .foregroundStyle(matchManager.currentlyDrawing ?
+                                 Color(red: 0.808, green: 0.345, blue: 0.776) :
+                                 Color(red: 0.243, green: 0.773, blue: 0.745)
+                            
+                            )
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, 1)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(
+            (matchManager.currentlyDrawing ?
+                Color(red: 0.243, green: 0.773, blue: 0.745) :
+                Color("primaryYellow")
+        )
+        .brightness(-0.2)
+        .opacity(0.5)
+    )
+    .clipShape(RoundedRectangle(cornerRadius: 20))
+    .padding(.vertical)
+    .padding(.bottom, 130)
+        
+        
     }
 }
 
