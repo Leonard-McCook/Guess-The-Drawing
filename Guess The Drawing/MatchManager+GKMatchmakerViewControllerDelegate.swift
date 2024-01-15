b4e075d7b61ff1f3753b7172e903ac4300820abc
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-15 16:36:09 -0500

    Create MatchmakerViewControllerDelegate

---
+++ Guess The Drawing/MatchManager+GKMatchmakerViewControllerDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  MatchManager+GKMatchmakerViewControllerDelegate.swift
+//  Guess The Drawing
+//
+//  Created by Leonard McCook-Carr on 1/15/24.
+//
+
+import Foundation
+import GameKit
+
+extension MatchManager: GKMatchmakerViewControllerDelegate {
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
+        viewController.dismiss(animated: true)
+        startGame(newMatch: match)
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFailWithError error: Error) {
+        viewController.dismiss(animated: true)
+    }
+    
+    func matchmakerViewControllerWasCancelled(_ viewController: GKMatchmakerViewController) {
+        viewController.dismiss(animated: true)
+    }
+}
